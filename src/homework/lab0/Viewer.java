fcc27a0e24748b7907682cd37bf7916bac593505
gsv2003 <gsv2003@gmail.com>
2016-07-29 20:54:04 +0300

    change Viewer.java

--- src/homework/lab0/Viewer.java
+++ src/homework/lab0/Viewer.java
@@ -8,21 +8,21 @@ public class Viewer {
     public static void main(String[] args) {
 
         Shape circle = new Circle();
-        System.out.println(circle.toString());
+        System.out.println(circle);
 
         Shape circle2 = new Circle(Color.GREEN, true, 9);
-        System.out.println(circle2.toString());
+        System.out.println(circle2);
 
         Shape rectangle = new Rectangle();
-        System.out.println(rectangle.toString());
+        System.out.println(rectangle);
 
         Shape rectangle2 = new Rectangle(Color.RED, true, 3, 5);
-        System.out.println(rectangle2.toString());
+        System.out.println(rectangle2);
 
         Shape square = new Square();
-        System.out.println(square.toString());
+        System.out.println(square);
 
         Rectangle square2 = new Square(Color.WHITE, true, 9);
-        System.out.println(square2.toString());
+        System.out.println(square2);
     }
 }
