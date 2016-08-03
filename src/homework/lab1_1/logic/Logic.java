f1d04aaa2a5ed15ab5f232ec7a557a2652033e5e
gsv2003 <gsv2003@gmail.com>
2016-08-03 21:15:36 +0300

    добавил "решение" первого задания

---
+++ src/homework/lab1_1/logic/Logic.java
@@ -0,0 +1,12 @@
+package homework.lab1_1.logic;
+
+/**
+ * Created by user on 03.08.16.
+ */
+
+public class Logic {
+    public String method(){
+        return "I am string in Logic.";
+    }
+
+}
---
+++ src/homework/lab1_1/main/Main.java
@@ -0,0 +1,14 @@
+package homework.lab1_1.main;
+
+/**
+ * Created by user on 03.08.16.
+ */
+import homework.lab1_1.logic.Logic;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
+}
