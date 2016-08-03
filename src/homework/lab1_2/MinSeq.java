8acbefbfab88daeb4280163db428ba76935d4e5a
gsv2003 <gsv2003@gmail.com>
2016-08-03 12:43:38 +0300

    completed lab1_2

---
+++ src/homework/lab1_2/MinSeq.java
@@ -0,0 +1,30 @@
+package homework.lab1_2;
+
+/**
+ * Created by user on 03.08.16.
+ */
+public class MinSeq {
+
+    private double limit;
+    private static double a;
+
+    public static void findMin(double limit)
+    {
+        for (int n = 0; ; n++)
+        {
+            a = 1 / Math.pow(n+1,2);
+            if (a < limit)
+            {
+                System.out.println("Наименьший элемент последовательности: " + n);
+                return;
+            }else
+            {
+                System.out.println("Номер элемента последовательности: " + n + " вычисленное значение: " + a);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        findMin(.04);
+    }
+}
